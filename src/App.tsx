eb5abb73d0242366c3f90fc365f9caf4cbf33407
Miguel Arduz <arduz.miguel@gmail.com>
2021-05-13 17:07:38 +0200

    Edit to trigger PWA reload alert

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <p>This is the placeholder for compaz PWA</p>
+      <p>Here we will start building compaz PWA</p>
       <a
         className="App-link"
         href="https://reactjs.org"
