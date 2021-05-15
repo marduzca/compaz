ddd7764650d9929d6ffb20e5fe20cd88ca1f07fa
Miguel Arduz <arduz.miguel@gmail.com>
2021-05-15 14:41:45 +0200

    Add logo to header and start header styling

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { ReactComponent as LogoBlack } from '../../static/img/logo_black.svg';
 import styles from './Header.module.css';
+
+interface HeaderItemProps {
+  content: string;
+}
+
+const HeaderItem: React.FC<HeaderItemProps> = (props) => (
+  // eslint-disable-next-line react/prop-types,react/destructuring-assignment
+  <div className={styles.menuItem}>{props.content}</div>
+);
 
 const Header: React.FC = () => (
   <header className={styles.header}>
-    <p>LOGO</p>
-    <p>DROPDOWN</p>
-    <p>Historial</p>
-    <p>Como instalar</p>
-    <p>Contacto</p>
-    <p>Quienes somos</p>
+    <div className={styles.logo}>
+      <LogoBlack />
+    </div>
+    <HeaderItem content="DROPDOWN" />
+    <HeaderItem content="Historial" />
+    <HeaderItem content="Como instalar" />
+    <HeaderItem content="Contacto" />
+    <HeaderItem content="Quienes somos" />
   </header>
 );
 export default Header;
