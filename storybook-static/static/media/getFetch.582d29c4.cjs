af1aaf0b120f65f543bd619cdd64855eff90b103
Miguel Arduz <arduz.miguel@gmail.com>
2021-07-17 22:07:07 +0200

    update deps

--- storybook-static/static/media/getFetch.582d29c4.cjs
+++ storybook-static/static/media/getFetch.582d29c4.cjs
@@ -1,15 +1,1 @@
-var fetchApi
-if (typeof fetch === 'function') {
-  if (typeof global !== 'undefined' && global.fetch) {
-    fetchApi = global.fetch
-  } else if (typeof window !== 'undefined' && window.fetch) {
-    fetchApi = window.fetch
-  }
-}
-
-if (typeof require !== 'undefined' && (typeof window === 'undefined' || typeof window.document === 'undefined')) {
-  var f = fetchApi || require('cross-fetch')
-  if (f.default) f = f.default
-  exports.default = f
-  module.exports = exports.default
-}
+var fetchApi;if("function"==typeof fetch&&("undefined"!=typeof global&&global.fetch?fetchApi=global.fetch:"undefined"!=typeof window&&window.fetch&&(fetchApi=window.fetch)),"undefined"!=typeof require&&("undefined"==typeof window||void 0===window.document)){var f=fetchApi||require("cross-fetch");f.default&&(f=f.default),exports.default=f,module.exports=exports.default}
