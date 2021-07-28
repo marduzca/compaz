418d77076e6545fce4ef7868a554c47b80668067
Miguel Arduz <arduz.miguel@gmail.com>
2021-07-28 23:28:36 +0200

    Add icons to mobile menu

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -2,6 +2,9 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { ReactComponent as BackIcon } from '../../static/img/arrow_back.svg';
+import { ReactComponent as HistoryIcon } from '../../static/img/history.svg';
+import { ReactComponent as InstallIcon } from '../../static/img/install.svg';
+import { ReactComponent as ContactIcon } from '../../static/img/contact.svg';
 import { ReactComponent as LogoBlack } from '../../static/img/logo_black.svg';
 import { ReactComponent as FlagUSA } from '../../static/img/flag_usa.svg';
 import { ReactComponent as FlagBolivia } from '../../static/img/flag_bolivia.svg';
@@ -10,10 +13,14 @@ import i18n from '../../i18n/instance';
 
 interface HeaderItemProps {
   content: string;
+  icon: JSX.Element;
 }
 
 const HeaderItem: React.FC<HeaderItemProps> = (props) => (
-  <span className={styles.headerItem}>{props.content}</span>
+  <div className={styles.headerItem}>
+    <span className={styles.headerItemIcon}>{props.icon}</span>
+    <span>{props.content}</span>
+  </div>
 );
 
 interface HeaderProps {
@@ -36,8 +43,8 @@ const Header: React.FC<HeaderProps> = (props) => {
 
   return (
     <header
-      className={`${!props.showMenuOnMobile ? styles.hiddenMenu : ''} ${
-        styles.header
+      className={`${styles.header} ${
+        !props.showMenuOnMobile ? styles.hiddenMenu : ''
       }`}
     >
       <button
@@ -64,17 +71,22 @@ const Header: React.FC<HeaderProps> = (props) => {
         <LogoBlack />
       </button>
       <div className={styles.headerItems}>
-        <HeaderItem content={t('Header.HISTORY')} />
-        <HeaderItem content={t('Header.HOW_TO_INSTALL')} />
-        <HeaderItem content={t('Header.CONTACT')} />
-        <button
-          title="language-selector"
-          type="button"
-          onClick={props.onLanguageChange}
-          className={styles.languageSelector}
-        >
-          {i18n.language.match(/en/i) ? <FlagBolivia /> : <FlagUSA />}
-        </button>
+        <HeaderItem content={t('Header.HISTORY')} icon={<HistoryIcon />} />
+        <HeaderItem
+          content={t('Header.HOW_TO_INSTALL')}
+          icon={<InstallIcon />}
+        />
+        <HeaderItem content={t('Header.CONTACT')} icon={<ContactIcon />} />
+        <div className={styles.languageSelector}>
+          <span>{t('Header.LANGUAGE')}</span>
+          <button
+            title="language-selector"
+            type="button"
+            onClick={props.onLanguageChange}
+          >
+            {i18n.language.match(/en/i) ? <FlagBolivia /> : <FlagUSA />}
+          </button>
+        </div>
       </div>
     </header>
   );
