230d24a613ea17086ccb55447f043d6dcff96554
Miguel Arduz <arduz.miguel@gmail.com>
2021-07-29 23:37:47 +0200

    Add landscape error message

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,46 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import styles from './App.module.css';
 import HeaderContainer from './components/header/HeaderContainer';
 import FooterContainer from './components/footer/FooterContainer';
 import PageContent from './components/content/PageContent';
 import { FirebaseProvider } from './components/providers/FirebaseProvider';
+import SadDogImage from './static/img/sad_dog.jpg';
+
+const LandscapeErrorMessage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={styles.landscapeMessage}>
+      <p>{t('LANDSCAPE_ERROR')}</p>
+      <img alt="Sad dog" src={SadDogImage} />
+    </div>
+  );
+};
 
 const App = () => {
   const [showMenuOnMobile, setShowMenuOnMobile] = useState<boolean>(false);
 
   return (
-    <div className={styles.App}>
-      <HeaderContainer
-        showMenuOnMobile={showMenuOnMobile}
-        onBackButtonClick={() => {
-          setShowMenuOnMobile(false);
-        }}
-      />
-      <FirebaseProvider>
-        <PageContent
-          onMenuButtonClick={() => {
-            setShowMenuOnMobile(true);
+    <>
+      <LandscapeErrorMessage />
+      <div className={styles.App}>
+        <HeaderContainer
+          showMenuOnMobile={showMenuOnMobile}
+          onBackButtonClick={() => {
+            setShowMenuOnMobile(false);
           }}
         />
-      </FirebaseProvider>
-      <FooterContainer />
-    </div>
+        <FirebaseProvider>
+          <PageContent
+            onMenuButtonClick={() => {
+              setShowMenuOnMobile(true);
+            }}
+          />
+        </FirebaseProvider>
+        <FooterContainer />
+      </div>
+    </>
   );
 };
 
