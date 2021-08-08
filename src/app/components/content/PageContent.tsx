0c8f2ba90f2e3b7e7d23e5f689c41a41ba1df722
Miguel Arduz <arduz.miguel@gmail.com>
2021-08-08 20:06:21 +0200

    Restructure app height and add map placeholder

--- src/app/components/content/PageContent.tsx
+++ src/app/components/content/PageContent.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MapImage from '../../static/img/fake_map.png';
 import styles from './PageContent.module.css';
 import TripSelectorContainer from './TripSelectorContainer';
 
@@ -9,6 +10,7 @@ interface PageContentProps {
 const PageContent: React.FC<PageContentProps> = (props) => (
   <div className={styles.content}>
     <TripSelectorContainer onMenuButtonClick={props.onMenuButtonClick} />
+    <img className={styles.map} alt="Map" src={MapImage} />
   </div>
 );
 
