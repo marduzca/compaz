3b527845f3a38eb0bd171961cf67c72948259308
Miguel Arduz <arduz.miguel@gmail.com>
2021-08-31 17:08:12 +0200

    Improve shadow of mobile menu

--- src/app/components/menu/Menu.stories.tsx
+++ src/app/components/menu/Menu.stories.tsx
@@ -3,6 +3,12 @@ import { Meta } from '@storybook/react';
 import { actions } from '@storybook/addon-actions';
 import Menu from './Menu';
 
+const guaranteedSize = (child: JSX.Element): JSX.Element => (
+  <div style={{ height: '500px', width: '400px', display: 'flex' }}>
+    {child}
+  </div>
+);
+
 export default {
   title: 'Menu',
   component: Menu,
@@ -17,12 +23,13 @@ export const normalState = () => (
   />
 );
 
-export const mobileState = () => (
-  <Menu
-    onLanguageChange={actions('onLanguageChange').onLanguageChange}
-    onLogoClick={actions('onLogoClick').onLogoClick}
-    onHideMobileMenu={actions('onBackButtonClick').onBackButtonClick}
-    showMenuOnMobile
-    fixNightMessage
-  />
-);
+export const mobileState = () =>
+  guaranteedSize(
+    <Menu
+      onLanguageChange={actions('onLanguageChange').onLanguageChange}
+      onLogoClick={actions('onLogoClick').onLogoClick}
+      onHideMobileMenu={actions('onBackButtonClick').onBackButtonClick}
+      showMenuOnMobile
+      fixNightMessage
+    />
+  );
