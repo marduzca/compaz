467f7b1410d439fb935b43783073b8d503944239
Miguel Arduz <arduz.miguel@gmail.com>
2021-09-07 22:59:42 +0200

    Show telerico and transfer icons instead of pure text

--- src/app/components/content/routes/RoutesOverview.tsx
+++ src/app/components/content/routes/RoutesOverview.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ReactComponent as ArrowUpIcon } from '../../../static/img/chevron_up.svg';
 import { ReactComponent as ArrowDownIcon } from '../../../static/img/chevron_down.svg';
 import { ReactComponent as BackIcon } from '../../../static/img/arrow_back.svg';
+import blueTelefericoIcon from '../../../static/img/blue_teleferico.svg';
+import brownTelefericoIcon from '../../../static/img/brown_teleferico.svg';
+import greenTelefericoIcon from '../../../static/img/green_teleferico.svg';
+import lightBlueTelefericoIcon from '../../../static/img/light_blue_teleferico.svg';
+import orangeTelefericoIcon from '../../../static/img/orange_teleferico.svg';
+import purpleTelefericoIcon from '../../../static/img/purple_teleferico.svg';
+import redTelefericoIcon from '../../../static/img/red_teleferico.svg';
+import silverTelefericoIcon from '../../../static/img/silver_teleferico.svg';
+import whiteTelefericoIcon from '../../../static/img/white_teleferico.svg';
+import yellowTelefericoIcon from '../../../static/img/yellow_teleferico.svg';
+import transferIcon from '../../../static/img/double_arrow.svg';
 import styles from './RoutesOverview.module.css';
 import { Route } from '../../domain';
 
@@ -15,6 +26,58 @@ interface RoutesOverviewProps {
 
 const RoutesOverview: React.FC<RoutesOverviewProps> = (props) => {
   const { t } = useTranslation();
+
+  const getCorrespondingTelefericoIcon = (lineColor: string): string => {
+    switch (lineColor) {
+      case 'blue':
+        return blueTelefericoIcon;
+      case 'brown':
+        return brownTelefericoIcon;
+      case 'green':
+        return greenTelefericoIcon;
+      case 'light_blue':
+        return lightBlueTelefericoIcon;
+      case 'orange':
+        return orangeTelefericoIcon;
+      case 'purple':
+        return purpleTelefericoIcon;
+      case 'red':
+        return redTelefericoIcon;
+      case 'silver':
+        return silverTelefericoIcon;
+      case 'white':
+        return whiteTelefericoIcon;
+      case 'yellow':
+        return yellowTelefericoIcon;
+      default:
+        // TODO: handle error case
+        return '';
+    }
+  };
+
+  const renderRoute = (): ReactNode[] => {
+    const lineIcons = props.route.subRoutes.map((subRoute) => (
+      <img
+        key={`${subRoute.line}`}
+        src={getCorrespondingTelefericoIcon(subRoute.line)}
+        title={subRoute.line}
+        alt={subRoute.line}
+        className={styles.teleferico}
+      />
+    ));
+
+    for (let index = 0; index < lineIcons.length; index += 1) {
+      if (index % 2 === 1) {
+        lineIcons.splice(
+          index,
+          0,
+          <img src={transferIcon} alt="Transfer" className={styles.transfer} />
+        );
+      }
+    }
+
+    return lineIcons;
+  };
 
   return (
     <div className={styles.routesContainer}>
@@ -41,17 +104,20 @@ const RoutesOverview: React.FC<RoutesOverviewProps> = (props) => {
           <ArrowUpIcon />
           <span>{t('Content.RoutesOverview.EARLIER_BUTTON')}</span>
         </button>
-        <section>
-          {props.route.subRoutes.map((subRoute, index) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <div key={`subRoute-${index}`}>
-              <p>Subroute in line {subRoute.line}</p>
-              <p>Takes {subRoute.totalTime} mins in total</p>
-              <p>V</p>
+        {props.route.subRoutes.length > 0 && (
+          <section className={styles.singleRouteOverview}>
+            <div className={styles.routeTop}>
+              <ul className={styles.route}>
+                {renderRoute().map((routeItem, index) => (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <li key={index}>{routeItem}</li>
+                ))}
+              </ul>
+              <span>{props.route.totalTime} min</span>
             </div>
-          ))}
-          <p>Total time of route is {props.route.totalTime}</p>
-        </section>
+            <p className={styles.routeClock}>15:00 - 15:36</p>
+          </section>
+        )}{' '}
         <button
           type="button"
           title={t('Content.RoutesOverview.LATER_BUTTON')}
