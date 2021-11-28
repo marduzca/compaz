258ab222f8da35e813132c4ac86efdfa645b1786
Miguel Arduz <arduz.miguel@gmail.com>
2021-11-28 15:30:08 +0100

    Use new Github action to deploy to Firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,6 @@
 import * as functions from 'firebase-functions';
 
-const helloWorld = functions.https.onRequest((request, response) => {
+export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true });
   response.send('Hello from Firebase!');
 });
-
-export default helloWorld;
