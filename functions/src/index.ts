efbffaf5ce27d915f06fb1d50d197cb533b77f9f
Miguel Arduz <arduz.miguel@gmail.com>
2021-11-28 16:54:43 +0100

    Allow CORS in test function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from 'firebase-functions';
 
 export const helloWorld = functions.https.onRequest((request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+
   functions.logger.info('Hello logs!', { structuredData: true });
   response.send('Hello from Firebase!');
 });
