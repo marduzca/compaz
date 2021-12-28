f9f0f9633e90de44d5da3eeb3fb3d847ab8870db
Miguel Arduz <arduz.miguel@gmail.com>
2021-12-28 18:47:24 +0100

    Remove unnecessary guaranteedSize in story

--- src/app/components/content/routes/routeDetails/RouteDetailsView.stories.tsx
+++ src/app/components/content/routes/routeDetails/RouteDetailsView.stories.tsx
@@ -4,95 +4,83 @@ import { actions } from '@storybook/addon-actions';
 import RouteDetailsView from './RouteDetailsView';
 import { ConnectedStation, SubRoute } from '../../../domain';
 
-const guaranteedSize = (child: JSX.Element): JSX.Element => (
-  <div
-    style={{
-      height: '615px',
-      width: '1500px',
-    }}
-  >
-    {child}
-  </div>
-);
-
 export default {
   title: 'RouteDetailsView',
   component: RouteDetailsView,
 } as Meta;
 
-export const basicInNormalState = () =>
-  guaranteedSize(
-    <RouteDetailsView
-      isMobile={false}
-      route={{
-        subRoutes: [
-          {
-            stationsPath: [
-              {
-                id: 'station_a',
-                name: 'Origin Station',
-                lines: ['purple'],
-                connectedStations: [
-                  { id: 'station_a_half', timeTo: 2 } as ConnectedStation,
-                ],
-              },
-              {
-                id: 'station_a_half',
-                name: 'A.5 Station',
-                lines: ['purple'],
-                connectedStations: [
-                  { id: 'station_b', timeTo: 4 } as ConnectedStation,
-                ],
-              },
-              {
-                id: 'station_b',
-                name: 'Intermediate Station',
-                lines: ['purple', 'blue'],
-                connectedStations: [
-                  { id: 'station_c', timeTo: 2 } as ConnectedStation,
-                ],
-              },
-            ],
-            totalTime: 6,
-            line: 'purple',
-            direction: 'End Station Purple Line',
-            transferTimeToNextLine: 3,
-          },
-          {
-            stationsPath: [
-              {
-                id: 'station_b',
-                name: 'Intermediate Station',
-                lines: ['blue', 'purple'],
-                connectedStations: [
-                  { id: 'station_c', timeTo: 2 } as ConnectedStation,
-                ],
-              },
-              {
-                id: 'station_c',
-                name: 'Destination Station',
-                lines: ['blue'],
-                connectedStations: [
-                  { id: 'station_d', timeTo: 2 } as ConnectedStation,
-                ],
-              },
-            ],
-            totalTime: 2,
-            line: 'blue',
-            direction: 'Start Station Blue Line',
-          },
-        ] as SubRoute[],
-        totalTime: 6,
-      }}
-      departureTime={new Date('1993-03-15 09:30')}
-      linesWithOpenIntermediateStations={['purple']}
-      onBackButtonClick={actions('onBackButtonClick').onBackButtonClick}
-      onIntermediateStationsButtonClick={
-        actions('onIntermediateStationsButtonClick')
-          .onIntermediateStationsButtonClick
-      }
-    />
-  );
+export const basicInNormalState = () => (
+  <RouteDetailsView
+    isMobile={false}
+    route={{
+      subRoutes: [
+        {
+          stationsPath: [
+            {
+              id: 'station_a',
+              name: 'Origin Station',
+              lines: ['purple'],
+              connectedStations: [
+                { id: 'station_a_half', timeTo: 2 } as ConnectedStation,
+              ],
+            },
+            {
+              id: 'station_a_half',
+              name: 'A.5 Station',
+              lines: ['purple'],
+              connectedStations: [
+                { id: 'station_b', timeTo: 4 } as ConnectedStation,
+              ],
+            },
+            {
+              id: 'station_b',
+              name: 'Intermediate Station',
+              lines: ['purple', 'blue'],
+              connectedStations: [
+                { id: 'station_c', timeTo: 2 } as ConnectedStation,
+              ],
+            },
+          ],
+          totalTime: 6,
+          line: 'purple',
+          direction: 'End Station Purple Line',
+          transferTimeToNextLine: 3,
+        },
+        {
+          stationsPath: [
+            {
+              id: 'station_b',
+              name: 'Intermediate Station',
+              lines: ['blue', 'purple'],
+              connectedStations: [
+                { id: 'station_c', timeTo: 2 } as ConnectedStation,
+              ],
+            },
+            {
+              id: 'station_c',
+              name: 'Destination Station',
+              lines: ['blue'],
+              connectedStations: [
+                { id: 'station_d', timeTo: 2 } as ConnectedStation,
+              ],
+            },
+          ],
+          totalTime: 2,
+          line: 'blue',
+          direction: 'Start Station Blue Line',
+        },
+      ] as SubRoute[],
+      totalTime: 6,
+    }}
+    departureTime={new Date('1993-03-15 09:30')}
+    linesWithOpenIntermediateStations={['purple']}
+    onBackButtonClick={actions('onBackButtonClick').onBackButtonClick}
+    onIntermediateStationsButtonClick={
+      actions('onIntermediateStationsButtonClick')
+        .onIntermediateStationsButtonClick
+    }
+  />
+);
 
 export const basicInMobileState = () => (
   <RouteDetailsView
