6de654b8f50ec53d63b6bcc8ab9173e969213d2f
Miguel Arduz <arduz.miguel@gmail.com>
2022-06-12 13:11:23 +0200

    Make linter happy for cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   viewportHeight: 900,
@@ -10,8 +10,9 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // eslint-disable-next-line @typescript-eslint/no-var-requires,global-require
+      return require('./cypress/plugins/index')(on, config);
     },
     baseUrl: 'http://localhost:3000',
   },
-})
+});
