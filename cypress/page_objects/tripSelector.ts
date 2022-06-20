218dad681b371e0b1e7b740cf73524b8a393c02b
Miguel Arduz <arduz.miguel@gmail.com>
2022-06-20 11:05:21 +0200

    Try to make e2e test less flaky v2

--- cypress/page_objects/tripSelector.ts
+++ cypress/page_objects/tripSelector.ts
@@ -1,8 +1,10 @@
 import Page from './page';
 
 const selectStations = (origin: string, destination: string) => {
-  Page.typeInField('Origin', `${origin}{downarrow}{enter}`);
-  Page.typeInField('Destination', `${destination}{downarrow}{enter}`);
+  Page.typeInField('Origin', origin);
+  Page.typeInField('Origin', '{downarrow}{enter}');
+  Page.typeInField('Destination', destination);
+  Page.typeInField('Destination', '{downarrow}{enter}');
 };
 
 const selectDateAndTime = (date: string, time: string) => {
