e3e1a6fb3061924e2f19962fb4ca04cddb08109b
Miguel Arduz <arduz.miguel@gmail.com>
2022-06-24 17:08:59 +0200

    Add translations to Instructions section

--- src/app/components/howToInstall/instructions/Instructions.tsx
+++ src/app/components/howToInstall/instructions/Instructions.tsx
@@ -1,65 +1,63 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import styles from './Instructions.module.css';
 import install from '../../../static/gif/install.gif';
 
-const Instructions: React.FC = () => (
-  <section className={styles.container} aria-labelledby="instructions">
-    <h2 id="instructions">Instructions</h2>
-    <div className={styles.content}>
-      <header className={styles.selectors}>
-        <label>
-          <span>Device</span>
-          <select>
-            <option>Smartphone / Tablet</option>
-            <option>Laptop</option>
-          </select>
-        </label>
-        <label>
-          <span>Browser</span>
-          <select id="device">
-            <option>Google Chrome</option>
-            <option>Mozilla Firefox</option>
-            <option>Safari</option>
-          </select>
-        </label>
-      </header>
-      <div className={styles.steps}>
-        {!localStorage.getItem('replaceGifForVisualRegressionTest') ? (
-          <img
-            src={install}
-            alt="Animation showing installations process"
-            className={styles.installationGif}
-          />
-        ) : (
-          <div
-            role="img"
-            aria-label="FAKE GIF"
-            className={`${styles.installationGif} ${styles.fakeGif}`}
-          >
-            FAKE GIF
-          </div>
-        )}
-        <ol>
-          <li>
-            Go to compaz.com and click the Install icon on the address bar.
-          </li>
-          <li>In the newly opened window, confirm by clicking `Install`</li>
-          <li>
-            The compaz icon is now visible in your desktop as an installed app.
-            Click the icon to open it.
-          </li>
-          <li>
-            You can now see compaz running with the look-and-feel of a native
-            app and enjoy all the benefits that come with it!
-          </li>
-        </ol>
-      </div>
-    </div>
-  </section>
-);
+const Instructions: React.FC = () => {
+  const { t } = useTranslation();
 
-Instructions.defaultProps = {
-  forVisualRegressionTest: false,
+  return (
+    <section className={styles.container} aria-labelledby="instructions">
+      <h2 id="instructions">{t('HowToInstall.Instructions.HEADING')}</h2>
+      <div className={styles.content}>
+        <header className={styles.selectors}>
+          <label>
+            <span>{t('HowToInstall.Instructions.DEVICE_SELECTOR_LABEL')}</span>
+            <select>
+              <option>
+                {t('HowToInstall.Instructions.SMARTPHONE_TABLET_OPTION')}
+              </option>
+              <option>{t('HowToInstall.Instructions.LAPTOP_OPTION')}</option>
+            </select>
+          </label>
+          <label>
+            <span>{t('HowToInstall.Instructions.BROWSER_SELECTOR_LABEL')}</span>
+            <select id="device">
+              <option>Google Chrome</option>
+              <option>Mozilla Firefox</option>
+              <option>Safari</option>
+            </select>
+          </label>
+        </header>
+        <div className={styles.steps}>
+          {!localStorage.getItem('replaceGifForVisualRegressionTest') ? (
+            <img
+              src={install}
+              alt={t('HowToInstall.Instructions.INSTALLATION_GIF_ALT', {
+                device: t('HowToInstall.Instructions.LAPTOP_OPTION'),
+                browser: 'Google Chrome',
+              })}
+              className={styles.installationGif}
+            />
+          ) : (
+            <div
+              role="img"
+              aria-label="FAKE GIF"
+              className={`${styles.installationGif} ${styles.fakeGif}`}
+            >
+              FAKE GIF
+            </div>
+          )}
+          <ol>
+            <li>{t('HowToInstall.Instructions.STEP_1_LAPTOP')}</li>
+            <li>{t('HowToInstall.Instructions.STEP_2_LAPTOP')}</li>
+            <li>{t('HowToInstall.Instructions.STEP_3')}</li>
+            <li>{t('HowToInstall.Instructions.STEP_4')}</li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Instructions;
