9fe8e8aff813da10171d511fa28b8c3552c47fe1
Miguel Arduz <arduz.miguel@gmail.com>
2022-12-18 17:44:30 -0400

    Add translations to introduction text

--- src/app/components/pages/contact/introduction/Introduction.tsx
+++ src/app/components/pages/contact/introduction/Introduction.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import sketch from '../../../../static/img/sketch.png';
 import { ReactComponent as GithubIcon } from '../../../../static/svg/github.svg';
 import { ReactComponent as LinkedinIcon } from '../../../../static/svg/linkedin.svg';
@@ -20,31 +20,18 @@ const Introduction: React.FC = () => {
       </div>
       <div className={styles.text}>
         <p>
-          Hola, mi nombre es Miguel Arduz y soy el creador de
-          <span className={styles.compaz}> compaz</span>. Nacido y crecido en la
-          ciudad de La Paz, me mudé a Alemania a los 19 años para realizar mis
-          estudios universitarios en la Universidad de Mannheim. Actualmente
-          vivo en Berlín y trabajo como consultor/programador en la consultora
-          Thoughtworks.
+          <Trans i18nKey="Contact.Introduction.PARAGRAPH_1">
+            Contact.Introduction.PARAGRAPH_1
+            <span className={styles.compaz} />
+          </Trans>
         </p>
         <p>
-          La idea de <span className={styles.compaz}> compaz</span> surge de mi
-          gran gusto por el teleférico y lo complicado que me parece manejarse
-          en él cada vez que estoy de visita en La Paz. Desarrollé la aplicación
-          dedicando mucho tiempo y esfuerzo para crear no solo algo que sirva,
-          sino también un producto de alta calidad. Me apasiona especialmente la
-          inclusión, por ende hice un esfuerzo extra en hacer a
-          <span className={styles.compaz}> compaz</span> accesible, de forma que
-          usuarios que requieran lectores de pantalla o solo usen el teclado no
-          deberían tener problemas para usar la aplicación. Este trabajo
-          representa mi regalo de agradecimiento por todo lo que me dió mi
-          ciudad.
+          <Trans i18nKey="Contact.Introduction.PARAGRAPH_2">
+            Contact.Introduction.PARAGRAPH_2
+            <span className={styles.compaz} />
+          </Trans>
         </p>
-        <p>
-          Espero que les guste y sobre todo, que les sea útil. Cualquier duda o
-          comentario no duden en contactarme a través del formulario en esta
-          misma página o por mis redes sociales.
-        </p>
+        <p>{t('Contact.Introduction.PARAGRAPH_3')}</p>
       </div>
       <div className={styles.socialNetworks}>
         <a
