19757e8d54b4314e1f270c2a9ab0302fbace0f9a
Miguel Arduz <arduz.miguel@gmail.com>
2023-01-19 12:57:52 -0400

    Finally found a way to keep the search button at the bottom without messing with the screen size

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import debounce from 'lodash.debounce';
 import { Route, Routes } from 'react-router-dom';
 import styles from './App.module.css';
 import MenuContainer from './components/organisms/menu/MenuContainer';
@@ -11,7 +10,6 @@ import { FirebaseProvider } from './components/providers/firebase/FirebaseProvid
 import { NavigationProvider } from './components/providers/navigation/NavigationProvider';
 import Contact from './components/pages/contact/Contact';
 import HowToInstall from './components/pages/howToInstall/HowToInstall';
-import useMediaQuery from './components/hooks/useMediaQuery';
 import { NavigationLink } from './components/organisms/menu/Menu';
 import About from './components/pages/about/About';
 import ErrorPage from './components/pages/error/ErrorPage';
@@ -31,40 +29,7 @@ const LandscapeErrorMessage = () => {
 };
 
 const App = () => {
-  const isMobile = useMediaQuery();
   const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false);
-
-  const initialHeight = window.innerHeight;
-
-  const adjustViewportHeight = () => {
-    const metaViewport = document.querySelector('meta[name=viewport]');
-
-    if (metaViewport) {
-      metaViewport.setAttribute(
-        'content',
-        `height=${initialHeight}, width=device-width, initial-scale=1`
-      );
-    }
-  };
-
-  const debouncedEventHandler = useMemo(
-    () => debounce(adjustViewportHeight, 75),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  );
-
-  useEffect(() => {
-    if (isMobile) {
-      window.addEventListener('resize', (event: Event) => {
-        // Only fix height if we are on portrait mode
-        if ((event.target as Window).innerHeight > 480) {
-          debouncedEventHandler();
-        }
-      });
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <>
