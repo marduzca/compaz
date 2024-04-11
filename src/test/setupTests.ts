4cd80298f1a59fb80f34e36e33b1aa115ffac8ff
Miguel Arduz <arduz.miguel@gmail.com>
2024-04-11 10:39:40 +0200

    Fix testing library type assertions usage

--- src/test/setupTests.ts
+++ src/test/setupTests.ts
@@ -2,6 +2,7 @@ import { vi } from 'vitest';
 import './i18nForTests';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@testing-library/jest-dom/vitest';
+import '@testing-library/jest-dom';
 
 window.matchMedia = () => ({
   matches: false,
