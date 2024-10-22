f3044b57d8d6a6cbfc6d90f5939302a5e388bda9
marduzca <arduz.miguel@gmail.com>
2024-10-22 15:16:38 +0200

    Improve SelectMui component to work with grouped options

--- src/app/components/atoms/select-mui/SelectMui.tsx
+++ src/app/components/atoms/select-mui/SelectMui.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   FormControl,
   InputLabel,
+  ListSubheader,
   MenuItem,
   Select,
   SelectChangeEvent,
@@ -9,10 +10,15 @@ import {
 import { Option } from '../select/Select';
 import styles from './SelectMui.module.css';
 
+export interface OptionsGroup {
+  label: string;
+  options: Option[];
+}
+
 export interface SelectMuiProps {
   onChange: (newOption: string) => void;
   selectedOption: string;
-  options: Option[];
+  options: OptionsGroup[] | Option[];
   label: string;
   isInitiallyOpen?: boolean;
 }
@@ -23,27 +29,46 @@ const SelectMui: React.FC<SelectMuiProps> = ({
   options,
   label,
   isInitiallyOpen = false,
-}) => (
-  <FormControl fullWidth>
-    <InputLabel id={`${label}-select-label`}>{label}</InputLabel>
-    <Select
-      labelId={`${label}-select-label`}
-      id={`${label}-select`}
-      value={selectedOption}
-      label={label}
-      onChange={(event: SelectChangeEvent) =>
-        onChange(event.target.value as string)
-      }
-      className={styles.select}
-      defaultOpen={isInitiallyOpen}
-    >
-      {options.map((option) => (
-        <MenuItem key={option.value} value={option.value}>
-          {option.text}
-        </MenuItem>
-      ))}
-    </Select>
-  </FormControl>
-);
+}) => {
+  const areOptionsGrouped = (options[0] as OptionsGroup).label !== undefined;
+
+  const renderSelectGroup = (group: OptionsGroup) => {
+    const optionsForGroup = group.options.map((p) => (
+      <MenuItem key={p.value} value={p.value}>
+        {p.text}
+      </MenuItem>
+    ));
+
+    return [
+      <ListSubheader key={group.label}>{group.label}</ListSubheader>,
+      ...optionsForGroup,
+    ];
+  };
+
+  return (
+    <FormControl fullWidth>
+      <InputLabel id={`${label}-select-label`}>{label}</InputLabel>
+      <Select
+        labelId={`${label}-select-label`}
+        id={`${label}-select`}
+        value={selectedOption}
+        label={label}
+        onChange={(event: SelectChangeEvent) =>
+          onChange(event.target.value as string)
+        }
+        className={styles.select}
+        defaultOpen={isInitiallyOpen}
+      >
+        {areOptionsGrouped
+          ? (options as OptionsGroup[]).map((group) => renderSelectGroup(group))
+          : (options as Option[]).map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.text}
+              </MenuItem>
+            ))}
+      </Select>
+    </FormControl>
+  );
+};
 
 export default SelectMui;
